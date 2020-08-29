8b7fbf4a94d112f32b63e7137259c6a6cacf086b
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-29 10:28:33 -0300

    Estrutura da api

---
+++ crud-mongoose/app.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+app.use(studentRouter);
+
+app.listen(3000, ()=> console.log('API Started'))
+
+// mongoose.connect('mongodb+srv://Mauricio:7468@bootcamp.ijmxh.mongodb.net/grades?retryWrites=true&w=majority', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }).then(
+
+//     console.log("Conectado ao banco Atlas")
+// ).catch(err => {
+//     console.log("Erro ao conectar ao banco de dados ", err);
+// });
+
+
