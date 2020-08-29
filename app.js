236dfca3ed0ee3dac7989e628fd06420e335943f
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-28 21:04:11 -0300

    conectando com o banco

---
+++ app.js
@@ -0,0 +1,18 @@
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://Mauricio:7468@bootcamp.ijmxh.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(async err => {
+    const collection = client.db("grades").collection("student");
+    const document = await collection.find().toArray();
+    console.log(document);
+
+    const databaselist = await client.db().admin().listDatabases();
+
+    console.log('Databases:');
+    databaselist.forEach(db => {
+        console.log(` - ${db.name}`);
+    });
+    // perform actions on the collection object
+    client.close();
+});
