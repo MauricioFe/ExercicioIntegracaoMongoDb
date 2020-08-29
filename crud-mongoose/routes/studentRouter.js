2c6adb9d74eb62214489fd6df2202139afb8b70d
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-29 10:34:31 -0300

    metodo post

---
+++ crud-mongoose/routes/studentRouter.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { studentModel } from './models/student.js'
+
+const app = express();
+
+app.post('/student', async (req, res) => {
+    try {
+        const student = new studentModel(req.body);
+        await student.save();
+        res.send(student)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+
+export { app as studentRouter };
